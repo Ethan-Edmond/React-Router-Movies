feadec92dcd5663e26f07764ca5fe0029271e3e3
Ethan-Edmond <eedmond@uccs.edu>
2021-05-17 14:18:53 -0600

    DRYed up the MovieDetails and Movie into MovieCard

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,5 +1,23 @@
 import React from 'react';
 
-export default function MovieCard (props) {
-  return;
+export default function MovieCard ({movie}) {
+  const {title, director, metascore, stars} = movie;
+  return (
+    <div className="movie-card">
+      <h2>{title}</h2>
+      <div className="movie-director">
+        Director: <em>{director}</em>
+      </div>
+      <div className="movie-metascore">
+        Metascore: <strong>{metascore}</strong>
+      </div>
+      {stars && <h3>Actors</h3>}
+      {stars && stars.map(star => {
+        return (<div key={star} className="movie-star">
+                  {star}
+                </div>
+               );
+      })}
+    </div>
+  );
 }
